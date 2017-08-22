8439fc6dc71a339d6ee13f246a6a8f5ba4899fdb
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-08-21 21:14:06 -0300

    copy command working

--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@
 import gi
 import sys
 import time
+import hashlib
+from subprocess import Popen, PIPE, STDOUT
 gi.require_version('Notify', '0.7')
 from gi.repository import GLib,Notify
 # One time initialization of libnotify
@@ -9,27 +11,39 @@ Notify.init("Remember")
 
 TIME_TO_LIVE_MS = 1000 * 60 * 2
 
-# Create the notification object
-# Define a callback function
-def hard(foo,bar,baz):
-    print("hard")
 
+def show_less(notification,bar,baz):
+    m = hashlib.md5()
+    m.update(str(notification.get_property('summary')).encode('utf-8'))
+    print(m.hexdigest())
 
-def easy(foo,bar,baz):
-    print("easy")
+def copy(notification,bar,baz):
+    print('copy')
+    p = Popen(['mycopy'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    p.communicate(input=str(notification.get_property('summary')).encode('utf-8'))[0]
+
+def listen(notification,bar,baz):
+    print('listen')
 
 notification = Notify.Notification.new(sys.stdin.read())
 notification.add_action(
-    "action_click",
-    "Show More",
-    hard,
+    "less",
+    "Show Less",
+    show_less,
     None # Arguments
 )
 
 notification.add_action(
-    "action_click",
-    "Show Less",
-    easy,
+    "copy",
+    "Copy",
+    copy,
+    None # Arguments
+)
+
+notification.add_action(
+    "listen",
+    "Listen",
+    listen,
     None # Arguments
 )
 # Actually show on screen
