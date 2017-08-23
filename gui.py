c1c671bb65f57f0298805ec9c777dc7d654e5276
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-08-22 21:28:56 -0300

    gui auto-run get-message

--- gui.py
+++ gui.py
@@ -3,10 +3,10 @@ import gi
 import sys
 import time
 import hashlib
+import subprocess
 from subprocess import Popen, PIPE, STDOUT
 gi.require_version('Notify', '0.7')
 from gi.repository import GLib,Notify
-# One time initialization of libnotify
 Notify.init("Remember")
 
 TIME_TO_LIVE_MS = 1000 * 60 * 2
@@ -25,42 +25,46 @@ def copy(notification,bar,baz):
     p.communicate(input=str(notification.get_property('summary')).encode('utf-8'))[0]
 
 def listen(notification,bar,baz):
-    p = Popen(['run_function', 'play_cached_voice'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    p = Popen(['/home/jean/projects/personal-scripts/run_function', 'play_cached_voice'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
     p.communicate(input=str(notification.get_property('summary')).encode('utf-8'))[0]
 
 def google(notification,bar,baz):
-    p = Popen(['google-it'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    p = Popen(['/home/jean/projects/personal-scripts/google_it'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
     p.communicate(input=str(notification.get_property('summary')).encode('utf-8'))[0]
 
-notification = Notify.Notification.new(sys.stdin.read())
+
+result = subprocess.run(['/home/jean/projects/remember_random/get_remember.sh'], stdout=subprocess.PIPE)
+message = result.stdout
+
+notification = Notify.Notification.new(str(message.decode('UTF-8')))
 notification.add_action(
     "less",
     "Show Less",
     show_less,
-    None # Arguments
+    None
 )
 
 notification.add_action(
     "copy",
     "Copy",
     copy,
-    None # Arguments
+    None
 )
 
 notification.add_action(
     "listen",
     "Listen",
     listen,
-    None # Arguments
+    None
 )
 
 notification.add_action(
     "google",
     "Google",
     google,
-    None # Arguments
+    None
 )
-# Actually show on screen
+
 notification.set_timeout(TIME_TO_LIVE_MS)
 notification.show()
 
