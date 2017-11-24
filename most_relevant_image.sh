2227601c8e927fbeb4cdcec8bb57b81c7eaa9d84
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-11-24 13:27:57 -0200

    tmp-file function  added

--- most_relevant_image.sh
+++ most_relevant_image.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+tmp-file() {
+echo /tmp/$(date "+%Y-%m-%d_%H-%I-%S")
+}
+
 remote=$(image_search.js  "$1" | head -n 1)
 tmp_file=$(tmp-file)
 wget $remote -q -o /dev/null -O $tmp_file
